3429515429af3dbd824b9fa8ba8f5622a42e8942
Pantheon-Technology <pantheontechnology2022@gmail.com>
2022-08-08 15:16:28 +0100

    V1.1

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const inputBtn = document.getElementById("input-btn");
-let myLeads = [];
+let myLeads = ["https://www.pantheontechnology.co.uk/"];
 const inputEl = document.getElementById("input-el");
 const saved = document.getElementById("saved-el");
 let msg = "";
+localStorage.setItem("leads", JSON.stringify(myLeads));
+
+
+function onStart(){
+
+    myLeads = localStorage.getItem("leads");
+    myLeads = JSON.parse(myLeads);
+
+     for (i = 0; i < myLeads.length; i++){
+
+         msg += 
+          `<li><a target='_blank' href="${myLeads[i]}">${myLeads[i]}</a></li>`;
+         saved.innerHTML = msg;
+     }
+}
+onStart();
 
 inputBtn.addEventListener("click", function(){
 
-    let userInput = inputEl.value;
-    myLeads.push(userInput);
-    inputEl.value = "";
     renderLeads();
+
 })
 
 function renderLeads(){
+
+    let userInput = inputEl.value;
+    myLeads.push(userInput);
+    myLeads = JSON.stringify(myLeads);
+    myLeads = JSON.parse(myLeads);
+    inputEl.value = "";
     msg = "";
 
     for (i = 0; i < myLeads.length; i++){
@@ -23,23 +43,36 @@ function renderLeads(){
             </a>
          </li>`;
          saved.innerHTML = msg;
-
-        // //DOES THE SAME...
-        // const li = document.createElement("li");
-        // li.textContent = myLeads[i];
-        //  saved.append(li);
     }
 }
 
+
+function clearAllLeads(){
+    localStorage.clear("leads");
+    saved.innerHTML = localStorage.getItem("leads");
+    let myLeads = [];
+}
+
+
+//LOCAL STORAGE PRACTICE
+
+// localStorage.setItem("Practice", "Hello!");
+// console.log(localStorage);
+// localStorage.clear();
+
+// localStorage.setItem("myLeads", "test");
+// console.log(localStorage.getItem("myLeads"));
+
+//END
 
 // let boxBtn = document.getElementById("box-el");
 // boxBtn.addEventListener("click", function(){
 //     console.log("I said don't press me!");
 // })
 
-//Practice
-const email = "Myles.hoult@gmail.com";
-const reciever = "JeffBezos@Amazon.com";
+// //Practice
+// const email = "Myles.hoult@gmail.com";
+// const reciever = "JeffBezos@Amazon.com";
 
-const EmailMsg = `Hey ${reciever} can I have your money? Sincerely ${email}`;
-console.log(EmailMsg);
+// const EmailMsg = `Hey ${reciever} can I have your money? Sincerely ${email}`;
+// console.log(EmailMsg);
